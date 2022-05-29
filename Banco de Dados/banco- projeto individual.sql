bb592fd7b651dcf8395853574fbc075e668897b2
MariaPaula-18 <maria.silva@sptech.school>
2022-05-29 19:52:51 -0300

    atualização do banco

--- Banco de Dados/banco- projeto individual.sql
+++ Banco de Dados/banco- projeto individual.sql
@@ -7,18 +7,23 @@ idUsuario int primary key auto_increment,
 nomeUsuario varchar (45),
 dataNacs date,
 email varchar (45),
-senha varchar (45),
-fkPremium int,
-foreign key (fkPremium) references usuario (idUsuario) );
+senha varchar (45));
 
 create table livro (
 idLivro int primary key auto_increment,
 nomeLivro varchar (45),
 dataLancamento date,
 nomeAutor varchar (45),
-editora varchar  (45),
+editora varchar  (45));
+
+create table usuarioLivro (
 fkUsuario int,
-foreign key (fkUsuario) references  usuario (idUsuario));
+fkLivro int,
+primary key (fkUsuario, fkLivro),
+foreign key (fkUsuario) references usuario (idUsuario),
+foreign key (fkLivro) references livro (idLivro),
+qtdPaginas decimal, 
+dataEntrada date );
 
 
 
